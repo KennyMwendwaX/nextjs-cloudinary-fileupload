a34716ab6e1c97c4fdf26ca7d47ae21414d54a41
IconicDexter <kennymwendwa67@gmail.com>
2023-07-04 11:25:39 +0300

    Finished the AddFile UI

--- components/AddFile.tsx
+++ components/AddFile.tsx
@@ -47,7 +47,7 @@ export default function AddFile() {
         onClick={handleModalToggle}
         className="mb-4 flex items-center rounded-lg border border-gray-400 bg-transparent px-5 py-2.5 text-sm font-medium text-gray-700 hover:border-gray-800 hover:bg-gray-800 hover:text-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200">
         <HiPlus className="mr-1 h-5 w-5" />
-        Add Note
+        Add File
       </button>
 
       {/* Modal */}
@@ -61,7 +61,9 @@ export default function AddFile() {
           <div className="relative w-full max-w-2xl rounded-lg bg-white p-4 shadow sm:p-5">
             {/* Modal header */}
             <div className="mb-4 flex items-center justify-between rounded-t border-b pb-4 sm:mb-5">
-              <h3 className="text-lg font-semibold text-gray-900">Add Note</h3>
+              <h3 className="text-lg font-semibold text-gray-900">
+                Upload File
+              </h3>
               <button
                 type="button"
                 onClick={handleModalToggle}
@@ -73,16 +75,26 @@ export default function AddFile() {
             </div>
             {/* Modal body */}
             <div>
-              <div className="mb-1 text-sm font-medium">Upload File</div>
-              <input
-                type="file"
-                className="text-grey-500 w-full cursor-pointer rounded-lg border-2 border-gray-900 bg-gray-100 pr-20 text-sm file:mr-5 file:border-0 file:bg-gray-900
+              <div>
+                <div className="mb-1 text-sm font-medium">Upload File</div>
+                <input
+                  type="file"
+                  className="text-grey-500 w-full cursor-pointer rounded-lg border-2 border-gray-900 bg-gray-50 pr-20 text-sm file:mr-5 file:border-0 file:bg-gray-900
              file:px-6 file:py-2 file:text-sm file:font-medium file:text-white hover:file:cursor-pointer"
-                onChange={handleFileChange}
-              />
-              <p className="mt-1 text-xs text-gray-500">
-                IMG, DOCX, PDF or mp3 (MAX. 800x800px).
-              </p>
+                  onChange={handleFileChange}
+                />
+                <p className="mt-1 text-xs text-gray-500">
+                  IMG, DOCX, PDF or mp3 (MAX. 800x800px).
+                </p>
+              </div>
+
+              <button
+                onClick={handleUpload}
+                disabled={uploading}
+                style={{ opacity: uploading ? ".5" : "1" }}
+                className="mt-4 w-32 rounded bg-red-600 py-2 text-center text-white">
+                {uploading ? "Uploading.." : "Upload"}
+              </button>
             </div>
           </div>
         </div>
