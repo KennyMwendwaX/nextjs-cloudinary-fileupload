ddd906b03285266ff050f1e48024bf420a8b36c9
IconicDexter <kennymwendwa67@gmail.com>
2023-07-07 21:46:11 +0300

    Added downloadCount to the db schema

--- prisma/migrations/20230707184546_dev/migration.sql
+++ prisma/migrations/20230707184546_dev/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "File" (
     "fileType" TEXT NOT NULL,
     "fileSize" INTEGER NOT NULL,
     "filePath" TEXT NOT NULL,
+    "downloadCount" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
