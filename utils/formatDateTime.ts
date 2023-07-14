4ccfcc907455dc43f1cbca6f7c4bbb0980955cf9
IconicDexter <kennymwendwa67@gmail.com>
2023-07-14 19:18:58 +0300

    Deleted getTimeAgo function

--- utils/formatDateTime.ts
+++ utils/formatDateTime.ts
@@ -1,5 +1,4 @@
 import format from "date-fns/format";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export function formatDateTime(timestamp: number): string {
   const fileDate = new Date(timestamp);
