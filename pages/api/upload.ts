d21e96854b686136b1dd1b6d16f1f0a3c7a1c2ce
IconicDexter <kennymwendwa67@gmail.com>
2023-08-27 15:02:42 +0300

    Added cloudinary

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
-import path from "path";
-import fs from "fs/promises";
+import { v2 as cloudinary } from "cloudinary";
 import { prisma } from "@/utils/db";
 
 export const config = {
@@ -52,6 +51,28 @@ const readFile = async (
   });
 };
 
+const getFormData = async (
+  req: NextApiRequest
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  const options: formidable.Options = {};
+  options.maxFileSize = 4000 * 1024 * 1024;
+  const form = formidable(options);
+
+  return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
+    (resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          // Access the fields under the "product" key
+          const productFields = JSON.parse(fields.product[0]);
+          resolve({ fields: productFields, files });
+        }
+      });
+    }
+  );
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -60,11 +81,34 @@ export default async function handler(
     return res.status(405).json({ message: "Method Not Allowed" });
 
   try {
-    await fs.readdir(path.join(process.cwd() + "/public", "/files"));
-  } catch (error) {
-    await fs.mkdir(path.join(process.cwd() + "/public", "/files"));
-  }
-  await readFile(req, true);
+    const { fields, files } = await getFormData(req);
 
-  res.status(201).json({ message: "File uploaded successfully" });
+    const myFiles = files.picture as formidable.File[];
+    const file = myFiles[0];
+
+    const uploadedImage = await cloudinary.uploader.upload(file.filepath, {
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+    });
+
+    if (!uploadedImage)
+      return res.status(500).json({ message: "Image upload failed" });
+
+    const file_uploaded = await prisma.file.create({
+      data: {
+        filename: file.newFilename,
+        fileType: file.mimetype as string,
+        fileSize: file.size,
+        filePath: file.filepath,
+      },
+    });
+
+    if (!file_uploaded) {
+      return res.status(500).json({ message: "File upload failed" });
+    } else {
+      return res.status(201).json({ message: "File uploaded successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
